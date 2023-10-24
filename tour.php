0d39182754d76deca18c9e154b73b6abddbc9fd0
Leah-Cervantes <cervantes.leamae@gmail.com>
2023-10-24 15:51:13 +0800

    tour: added note for PNRI Interactive Map

--- tour.php
+++ tour.php
@@ -557,7 +557,12 @@ bottom: 2.7em;
   
     <h2 style="margin: 0;text-align: right; " id="lbl_name"><?php if(isset($_SESSION['name'])) echo $_SESSION['name']; ?></h2>
     <h3 style="margin-top: 0; text-align: right;" id="lbl_email"><?php if(isset($_SESSION['email'])) echo $_SESSION['email']; ?></h3>
-    <div style="position: relative; width: 100%; height: 0; padding-top: 141.4286%; padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; border-radius: 8px; will-change: transform;">  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFTgdy8Ruw&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">  </iframe></div>
+    <!-- PNRI Interactive Map PDF-->
+    <div style="position: relative; width: 100%; height: 0; padding-top: 141.4286%; padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; border-radius: 8px; will-change: transform;">  
+      <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"    
+        src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFTgdy8Ruw&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">  
+      </iframe>
+    </div>
     <!--<img src="facilities/info.png" style="width: calc(100vw - 4em);max-width: 600px;">-->
   </div>
 
