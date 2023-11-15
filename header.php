c19cbe030e51230f3b441ecedc2843719a164c48
Lea Mae Cervantes <cervantes.leamae@gmail.com>
2023-11-15 21:46:14 +0800

    update banners, added animation

--- header.php
+++ header.php
@@ -33,7 +33,7 @@ $lastUriSegment = array_pop($uriSegments);
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-<link href="assets/css/zoom.css" rel="stylesheet" />
+<!-- <link href="assets/css/zoom.css" rel="stylesheet" /> -->
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;500&display=swap'); 
