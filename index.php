24d6918dfa15a5aba5fb51b893dd9a334fc16ac7
cabatanared <cabatabaredilyn1709@gmail.com>
2023-11-15 23:32:07 +0800

    PNSO video

--- index.php
+++ index.php
@@ -428,7 +428,9 @@ right: 0;">
     <div class="row" id="carusel">
     <!--<div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%; padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 0px; margin-bottom: 0px; overflow: hidden; border-radius: 8px; will-change: transform;">  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAE1-VaTqvI&#x2F;watch?embed" allowfullscreen="allowfullscreen" allow="fullscreen">  </iframe></div>-->
 
-    <div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%; padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 0px; margin-bottom: 0px; overflow: hidden; border-radius: 8px; will-change: transform;">  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFS1_ZcREQ&#x2F;watch?embed" allowfullscreen="allowfullscreen" allow="fullscreen">  </iframe></div>
+    <div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%; padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 0px; margin-bottom: 0px; overflow: hidden; border-radius: 8px; will-change: transform;"> 
+     <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;" src="assets/PNSO Logo-Anim_3.mp4" allowfullscreen="allowfullscreen" allow="fullscreen">  </iframe>
+    </div>
 
     <!--<div class="swiper">
 
