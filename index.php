671034301ec9b357370b8418d9ade451448b23d1
Leah-Cervantes <cervantes.leamae@gmail.com>
2023-11-21 15:50:59 +0800

    index: change menu bar color

--- index.php
+++ index.php
@@ -240,7 +240,7 @@ left: 167px;
         /* background: #eee; */
         font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
         font-size: 14px;
-        /* BAR ICON */
+        /* MENU BAR ICON */
         color: #9D1F45;
         margin: 0;
         padding: 0;
